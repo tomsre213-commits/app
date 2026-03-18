21f20ed604dc7215bd49dbfcff315f8cddef87c7
Darel James A. Narvasa <124035782+DarelJamesNarvasa@users.noreply.github.com>
2026-03-18 21:01:24 +0800

    sign page backend

--- lib/pages/dashboard/dashboard_page.dart
+++ lib/pages/dashboard/dashboard_page.dart
@@ -1,8 +1,23 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tindak/pages/auth/login_page.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
+
+  Future<void> _logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+
+    if (context.mounted) {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const LoginPage(),
+        ),
+            (route) => false,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,14 +25,13 @@ class DashboardPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: const Text("Dashboard"),
         actions: [
           IconButton(
             icon: const Icon(Icons.logout),
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-            },
-          )
+            onPressed: () => _logout(context),
+          ),
         ],
       ),
       body: Center(
